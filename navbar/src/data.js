1d1c4cfe815ea607abff042fbea2fef1dda1fce5
Musab Momin <musab.momin@outlook.com>
2021-10-07 23:14:19 +0530

    navbar project completed

---
+++ navbar/src/data.js
@@ -0,0 +1,51 @@
+const links = [
+    {
+      id: 1,
+      url: '/',
+      text: 'home',
+    },
+    {
+      id: 2,
+      url: '/about',
+      text: 'about',
+    },
+    {
+      id: 3,
+      url: '/projects',
+      text: 'projects',
+    },
+    {
+      id: 4,
+      url: '/contact',
+      text: 'contact',
+    },
+    {
+      id: 5,
+      url: '/profile',
+      text: 'profile',
+    },
+];
+const social = [
+  {
+    id: 1,
+    url: 'https://www.twitter.com',
+    text: <i class="fab fa-twitter fa-sm cls"></i>
+  },
+  {
+    id: 2,
+    url: 'https://www.twitter.com',
+    text: <i class="fab fa-linkedin fa-sm cls"></i> 
+  },
+  {
+    id: 3,
+    url: 'https://www.twitter.com',
+    text: <i class="fab fa-facebook fa-sm cls"></i>
+  },
+  {
+    id: 4,
+    url: 'https://www.twitter.com',
+    text: <i class="fab fa-instagram fa-sm cls"></i>
+  },
+];
+
+export {links, social};
